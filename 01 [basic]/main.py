ee4cc5df1f3567fbcebda83a6cfb076fe271eabc
abdur-shobur <abdur.shobur.me@gmail.com>
2025-11-25 12:44:03 +0600

    Day 1 : Basic start

---
+++ 01 [basic]/main.py
@@ -0,0 +1,26 @@
+# Run Hello World
+print("Hello, World!")
+
+# Variable
+name = "Abdur Shobur"
+age = 25
+isStudent = True
+
+
+# Operation
+a = 10
+b = 9
+
+print(a+b)
+print(a/b)
+print(a-b)
+print(a*b)
+
+# Comments
+"""
+Multi line comment 
+"""
+
+# input from user
+userInput = input("Enter Your Name...")
+print("hello "+userInput)
---
+++ 01 [basic]/readme.md
@@ -0,0 +1,19 @@
+# Day 1: Python Basics
+
+**What is Python?**
+
+- Python is a high-level, interpreted programming language.
+
+**Install**
+
+```bash
+python --version
+```
+
+**Basics**
+
+- write hello world
+- variable
+- operation
+- comment
+- user input
